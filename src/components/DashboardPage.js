5c8e34fce8e844dfd6b7c9b2a10ec3777dae9afd
Sergio <38168163+FundacionBlockchainChile@users.noreply.github.com>
2020-08-02 13:07:07 -0400

    Init commit

---
+++ src/components/DashboardPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const DashboardPage = () => (
+  <div>
+    Dashboard page content
+  </div>
+);
+
+export default DashboardPage;
+
+// Element example...
+// <div>
+//   <ExpensesSummary />
+//   <ExpenseListFilters />
+//   <ExpenseList />
+// </div>;
